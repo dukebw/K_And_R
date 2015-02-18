6a306d34359906aab19056cb0759dbf44247a501
Brendan Duke <dukebw@mcmaster.ca>
2015-02-18 10:07:53 -0500

    Moved folders around, trying to profile readlines

---
+++ Ch5/src/alloc.h
@@ -0,0 +1,32 @@
+#ifndef ALLOC_H
+#define ALLOC_H 
+// NOTE(brendan): size of available space
+#define ALLOCSIZE 10000000
+
+// NOTE(brendan): storage for alloc
+static char allocbuf[ALLOCSIZE];
+// NOTE(brendan): next free position
+static char *allocp = allocbuf;
+
+// NOTE(brendan): return pointer to n characters
+char *alloc(int n) {
+  // NOTE(brendan): it fits
+  if(allocbuf + ALLOCSIZE - allocp >= n) {
+    allocp += n;
+    // NOTE(brendan): old p
+    return (allocp - n);
+  }
+  else {
+    // NOTE(brendan): not enough room
+    return 0;
+  }
+}
+
+// NOTE(brendan): free storage pointed to by p
+void afree(char *p) {
+  if(p >= allocbuf && p < allocbuf + ALLOCSIZE) {
+    allocp = p;
+  }
+}
+
+#endif /* ALLOC_H */
---
+++ Ch5/src/sort_lines.cpp
@@ -0,0 +1,109 @@
+/* ========================================================================
+   File: sort_lines.cpp
+   Date: Feb. 17/15
+   Revision: 1
+   Creator: Brendan Duke
+   Notice: (C) Copyright 2015 by BRD Inc. All Rights Reserved.
+   ======================================================================== */
+
+#include <stdio.h>
+#include <string.h>
+#include "alloc.h"
+
+// NOTE(brendan): max #lines to be sorted
+#define MAXLINES 5000
+// NOTE(brendan): max length of any input line
+#define MAXLEN 10000
+
+// NOTE(brendan): pointers to text lines
+char *lineptr[MAXLINES];
+
+// NOTE(brendan): read a line into s; return length
+int getline(char s[], int lim) {
+	int c;
+
+  char *start = s;
+  while((lim-- > 0) && ((c=getchar()) != EOF) && (c != '\n')) {
+    *s++ = c;
+  }
+	if(c == '\n') {
+		*s++ = c;
+  }
+	*s = '\0';
+	return (s - start);
+}
+
+char *alloc(int);
+
+//NOTE(brendan): read input lines
+int readlines(char *lineptr[], int maxlines) {
+  int len, nlines = 0;
+  char *p, line[MAXLEN];
+
+  while((len = getline(line, MAXLEN)) > 0) {
+    if(nlines >= maxlines || (p = alloc(len)) == NULL) {
+      return -1;
+    }
+    else {
+      // NOTE(brendan): delete newline
+      line[len - 1] = '\0';
+      strcpy(p, line);
+      lineptr[nlines++] = p;
+    }
+  }
+  return nlines;
+}
+
+// NOTE(brendan): write output lines
+void writelines(char *lineptr[], int nlines) {
+  while(nlines-- > 0) {
+    printf("%s\n", *lineptr++);
+  }
+}
+
+// NOTE(brendan): interchange v[i] and v[j]
+void swap(char *v[], int i, int j) {
+  char *temp;
+
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+// NOTE(brendan): sort v[left...right] into increasing order
+void qsort(char *v[], int left, int right) {
+  // NOTE(brendan): do nothing if array contains fewer than two elements
+  if(left >= right) {
+    return;
+  }
+  swap(v, left, (left + right)/2);
+  int last = left;
+  for(int i = left + 1; i <= right; ++i) {
+    if(strcmp(v[i], v[left]) < 0) {
+      swap(v, ++last, i);
+    }
+  }
+  swap(v, left, last);
+  qsort(v, left, last - 1);
+  qsort(v, last + 1, right);
+}
+
+// NOTE(brendan): sort input lines
+int main() {
+  // NOTE(brendan): number of input lines read
+  int nlines;
+  // TODO(brendan): get a string from file and repeatedly pass it to this
+  // loop to do some profiling
+  for(int i = 0; i < 100; ++i) {
+    readlines(lineptr, MAXLINES);
+  }
+  if((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    qsort(lineptr, 0, nlines - 1);
+    writelines(lineptr, nlines);
+    return 0;
+  }
+  else {
+    printf("error: input too big to sort\n");
+    return 1;
+  }
+}
