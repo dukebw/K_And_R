e9bc7c086fc46e28b3c4c7de086e92e99a25c52f
Brendan Duke <dukebw@mcmaster.ca>
2015-04-12 09:29:08 -0400

    Added -r and -f (still working on -f) to generic_sort

--- Ch5/src/generic_sort.cpp
+++ Ch5/src/generic_sort.cpp
@@ -1,10 +1,10 @@
 /* ========================================================================
-File: generic_sort.cpp
-Date: Mar. 5/15
-Revision: 1
-Creator: Brendan Duke
-Notice: (C) Copyright 2015 by BRD Inc. All Rights Reserved.
-======================================================================== */
+   File: generic_sort.cpp
+   Date: Mar. 5/15
+   Revision: 1
+   Creator: Brendan Duke
+   Notice: (C) Copyright 2015 by BRD Inc. All Rights Reserved.
+   ======================================================================== */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +12,7 @@ Notice: (C) Copyright 2015 by BRD Inc. All Rights Reserved.
 #include "util.h" // NOTE(brendan): swap, readlines, writelines
 
 // NOTE(brendan): max #lines to be sorted
-#define MAXLINES 5000
+#define MAXLINES 4096
 
 // NOTE(brendan): pointers to text lines
 char *lineptr[MAXLINES];
@@ -35,6 +35,18 @@ int numcmp(char *s1, char *s2) {
     }
 }
 
+// NOTE(brendan): reverse of numcmp
+int reverseNumcmp(char *s1, char *s2)
+{
+    return -numcmp(s1, s2);
+}
+
+// NOTE(brendan): reverse of strcmp
+int reverseStrcmp(char *s1, char *s2)
+{
+    return -strcmp(s1, s2);
+}
+
 // NOTE(brendan): sort v[left]..v[right] into increasing order
 void qsort(void *v[], int left, int right, int (*comp)(void *, void *)) {
     void swap(void *v[], int, int); 
@@ -55,8 +67,43 @@ void qsort(void *v[], int left, int right, int (*comp)(void *, void *)) {
     qsort(v, last + 1, right, comp);
 }
 
-// TODO(brendan): modify program to handle a -r flag, which indicates
-// sorting in reverse (decreasing) order. Be sure that -r works with -n.
+// NOTE(brendan): INPUT: a character. OUTPUT: that character
+// in lower-case if it was in upper case; the input is output otherwise
+char foldCase(char c)
+{
+    if ((c >= 'A') && (c <= 'Z')) {
+        c += 'a' - 'A';
+    }
+    return c;
+}
+
+// NOTE(brendan): compare string cs to string ct;
+// return <0 if cs < ct, 0 if cs == ct, or >0 if cs > ct
+// treat upper-case and lower-case letters as equal
+int foldStrcmp(char *cs, char *ct) {
+    while (true) {
+        char csTemp = foldCase(*cs);
+        char ctTemp = foldCase(*ct);
+        if (csTemp < ctTemp) {
+            return -1;
+        }
+        else if (csTemp > ctTemp) {
+            return 1;
+        }
+        // NOTE(brendan): *cs == *ct
+        if (*cs == '\0') {
+            return 0;
+        }
+        ++cs;
+        ++ct;
+    }
+    return 0;
+}
+
+// TODO(brendan): add the option -f to fold upper and lower case together,
+// so that case distinctions are not made during sorting; for example,
+// a and A compare equal
+// TODO(brendan): foldStrcmp should put capital 'A' before 'a'
 // NOTE(brendan): sort input lines
 int main(int argc, char *argv[]) {
     // NOTE(brendan): number of input lines read
@@ -64,15 +111,46 @@ int main(int argc, char *argv[]) {
 
     //NOTE(brendan): 1 if numeric sort
     int numeric = 0;
-
-    if ((argc > 1) && (strcmp(argv[1], "-n") == 0)) {
-        numeric = 1;
+    int reverse = 0;
+    int fold = 0;
+    for (int argIndex = 1; argIndex < argc; ++argIndex) {
+        char *argString = argv[argIndex];
+        if (*argString != '-') {
+            printf("Error! file input not supported yet\n");
+            return 2;
+        }
+        while (*++argString != '\0') {
+            switch (*argString) {
+                case 'n':
+                {
+                    numeric = 1;
+                }   break;
+                case 'r':
+                {
+                    reverse = 1;
+                }   break;
+                case 'f':
+                {
+                    fold = 1;
+                }   break;
+                default:
+                {
+                    printf("Invalid input -- '%c'\n", *argString);
+                    return 2;
+                }
+            }
+        }
     }
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        qsort((void **)lineptr, 0, nlines - 1, 
-              (numeric ? 
-               (int (*)(void *, void *))numcmp : 
-               (int (*)(void *, void *))strcmp));
+        int (*pNumcmp)(void *, void *) = 
+            reverse ? (int (*)(void *, void *))reverseNumcmp : 
+                      (int (*)(void *, void *))numcmp;
+        int (*pStrcmp)(void *, void *) =
+            reverse ? (int (*)(void *, void *))reverseStrcmp :
+                      (int (*)(void *, void *))strcmp;
+        // TODO(brendan): reverse for fold
+        pStrcmp = fold ? (int (*)(void *, void*))foldStrcmp : pStrcmp;
+        qsort((void **)lineptr, 0, nlines - 1, (numeric ?  pNumcmp : pStrcmp));
         writelines(lineptr, nlines);
         return 0;
     }
--- Ch5/src/str_functions.cpp
+++ Ch5/src/str_functions.cpp
@@ -14,104 +14,104 @@
 
 // NOTE(brendan): copies the string t to the end of s
 char *strcat(char *s, char *t) {
-  char *result = s;
-  while(*s != '\0') {
-    ++s;
-  }
-  while((*s++ = *t++) != '\0');
-  return result;
+    char *result = s;
+    while (*s != '\0') {
+        ++s;
+    }
+    while ((*s++ = *t++) != '\0');
+    return result;
 }
 
 // NOTE(brendan): returns length of string s
 int strlen(char *s) {
-  char *sStart = s;
-  while(*s != '\0') {
-    ++s;
-  }
-  return ((s - sStart) / sizeof(char));
+    char *sStart = s;
+    while (*s != '\0') {
+        ++s;
+    }
+    return ((s - sStart) / sizeof(char));
 }
 
 // NOTE(brendan): swap the characters s and t (xor trick)
 void swap(char *s, char *t) {
-  *s ^= *t;
-  *t ^= *s;
-  *s ^= *t;
+    *s ^= *t;
+    *t ^= *s;
+    *s ^= *t;
 }
 
 // NOTE(brendan): reverses string s and returns pointer to start of s;
 // side-effects
 char *reverse(char *s) {
-  int sLength = strlen(s);
-  for(int i = 0; i < sLength/2; ++i) {
-    swap(s + i, s + (sLength - 1) - i);
-  }
-  return s;
+    int sLength = strlen(s);
+    for (int i = 0; i < sLength/2; ++i) {
+        swap(s + i, s + (sLength - 1) - i);
+    }
+    return s;
 }
 
 // NOTE(brendan): returns true if the string t occurs at the end of the string
 // s; false otherwise.
 bool strend(char *s, char *t) {
-  char *sReverse = reverse(s);
-  char *tReverse = reverse(t);
-  while(*tReverse != '\0') {
-    if(*sReverse != *tReverse) {
-      return false;
+    char *sReverse = reverse(s);
+    char *tReverse = reverse(t);
+    while (*tReverse != '\0') {
+        if (*sReverse != *tReverse) {
+            return false;
+        }
+        ++sReverse;
+        ++tReverse;
     }
-    ++sReverse;
-    ++tReverse;
-  }
-  return true;
+    return true;
 }
 
 // NOTE(brendan): copy at most n characters of string ct to s; return s.
 // Pad with '\0's if ct has fewer than n characters.
 char *strncpy(char *s, char *ct, int n) {
-  char *result = s;
-  while(((s - result) < n) && ((*s++ = *ct++) != '\0'));
-  while((s - result) < n) {
-    *s++ = '\0';
-  }
-  return result;
+    char *result = s;
+    while (((s - result) < n) && ((*s++ = *ct++) != '\0'));
+    while ((s - result) < n) {
+        *s++ = '\0';
+    }
+    return result;
 }
 
 // NOTE(brendan): concatenate at most n characters of string ct to string s;
 // terminate s with '\0'; return s.
 char *strncat(char *s, char *ct, int n) {
-  char *result = s;
-  while(*s != '\0') {
-    ++s;
-  }
-  char *start = s;
-  while(((s - start) < n) && ((*s++ = *ct++) != '\0'));
-  while((s - start) < n) {
-    *s++ = '\0';
-  }
-  return result;
+    char *result = s;
+    while (*s != '\0') {
+        ++s;
+    }
+    char *start = s;
+    while (((s - start) < n) && ((*s++ = *ct++) != '\0'));
+    while ((s - start) < n) {
+        *s++ = '\0';
+    }
+    return result;
 }
 
 // NOTE(brendan): compare at most n characters of string cs to string ct;
 // return <0 if cs < ct, 0 if cs == ct, or >0 if cs > ct
 int strncmp(char *cs, char *ct, int n) {
-  for(int i = 0; i < n; ++i) {
-    if(*cs < *ct) {
-      return -1;
+    for (int i = 0; i < n; ++i) {
+        if (*cs < *ct) {
+            return -1;
+        }
+        else if (*cs > *ct) {
+            return 1;
+        }
+        if (*cs == '\0') {
+            return 0;
+        }
+        // NOTE(brendan): *cs == *ct
+        ++cs;
+        ++ct;
     }
-    else if(*cs > *ct) {
-      return 1;
-    }
-    if(*cs == '\0') {
-      return 0;
-    }
-    // NOTE(brendan): *cs == *ct
-    ++cs;
-    ++ct;
-  }
-  return 0;
+    return 0;
 }
 
 int main() {
-  char testString0[MAXLINE] = "abcdeFghijklmnopqrstuvwxyz";
-  char testString1[] = "abcdefghijklmnopqrstuvwxyz";
-  char strendTest[] = "567890";
-  printf("%d\n", strncmp(testString0, testString1, 6));
+    char testString0[MAXLINE] = "abcdeFghijklmnopqrstuvwxyz";
+    char testString1[] = "abcdefghijklmnopqrstuvwxyz";
+    char strendTest[] = "567890";
+    printf("%d\n", strncmp(testString0, testString1, 6));
 }
